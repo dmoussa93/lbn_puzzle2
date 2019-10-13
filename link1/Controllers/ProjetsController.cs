7e2f177d1109e1b562de008016aece1b9d481363
Eugène Rodolphe MAZAMDA  TANGUEU <er.mazamdatangueu@linkbynet.com>
2019-10-13 11:36:11 +0200

    commit

---
+++ link1/Controllers/ProjetsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace link1.Controllers
+{
+    public class ProjetsController : Controller
+    {
+        // GET: Projets
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ link1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(link1.Startup))]
+namespace link1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
